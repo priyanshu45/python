811cf812e7caadd89720996cdb32cfb0f2776c1d
priyanshu45 <68326166+priyanshu45@users.noreply.github.com>
2020-11-05 21:03:34 +0530

    Add files via upload

---
+++ tut18.py
@@ -0,0 +1,10 @@
+while 1:
+    i = int(input("Please enter a number"))
+    if i < 100:
+        print("try again")
+        continue
+    else:
+         print("Congratulations!!!!!! you enter right number")
+         break
+
+
