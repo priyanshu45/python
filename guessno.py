4c1b040898e373f49da751213cf0ba47c91f1ad2
priyanshu45 <68326166+priyanshu45@users.noreply.github.com>
2020-11-07 16:59:10 +0530

    Create guessno.py

---
+++ guessno.py
@@ -0,0 +1,15 @@
+# do exercise
+var = input("Enter your name\n")
+print("hello",var)
+print("Do you want to play this game")
+var = input()
+if ("yes"):
+
+    var = "welcome"
+    print(var)
+var = input()
+
+
+
+
+
